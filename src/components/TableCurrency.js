65dcd828d193e82bce2156b0ac2c8961772493f6
Alexey Nelipovich <nelipovich1992@gmail.com>
2018-08-24 12:32:37 +0300

    Changed table

--- src/components/TableCurrency.js
+++ src/components/TableCurrency.js
@@ -1,10 +1,25 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 import currencyId from "../currencyId";
 
+const styleTable = {
+  border: "1px solid #000",
+  padding: "5px 10px",
+  textAlign: "center"
+};
+
 class TableCurrency extends Component {
+  state = {
+    currentCurrancy: "USD"
+  };
+
+  changeCurrentCurrency = e => {
+    this.setState({
+      currentCurrancy: e.target.value
+    });
+  };
+
   render() {
-    console.log(this.props.rate);
     return (
       <div>
         <div
@@ -17,7 +32,7 @@ class TableCurrency extends Component {
           <label>
             USD
             <input
-              onClick={this.props.changeCurrency}
+              onClick={this.changeCurrentCurrency}
               id={currencyId.USD}
               type="radio"
               name="chooseCurrency"
@@ -27,7 +42,7 @@ class TableCurrency extends Component {
           <label>
             EUR
             <input
-              onClick={this.props.changeCurrency}
+              onClick={this.changeCurrentCurrency}
               id={currencyId.EUR}
               type="radio"
               name="chooseCurrency"
@@ -37,7 +52,7 @@ class TableCurrency extends Component {
           <label>
             RUB
             <input
-              onClick={this.props.changeCurrency}
+              onClick={this.changeCurrentCurrency}
               id={currencyId.RUB}
               type="radio"
               name="chooseCurrency"
@@ -45,13 +60,25 @@ class TableCurrency extends Component {
             />
           </label>
         </div>
-        <h3>Table Caption</h3>
         <button onClick={this.getData}>+</button>
-        <ul>
-          {this.props.rate.map(item => (
-            <li>{item.Cur_OfficialRate}</li>
-          ))}
-        </ul>
+        <table style={{ borderCollapse: "collapse" }}>
+          <caption>Таблица</caption>
+          <tbody>
+            <tr>
+              <th colSpan="2" style={styleTable}>
+                {this.props.currencyAbbreviation}
+              </th>
+            </tr>
+            {this.props.rate.map((item, idx) => (
+              <Fragment key={idx}>
+                <tr>
+                  <td style={styleTable}>{item.Date.slice(0, 10)}</td>
+                  <td style={styleTable}>{item.Cur_OfficialRate}</td>
+                </tr>
+              </Fragment>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
